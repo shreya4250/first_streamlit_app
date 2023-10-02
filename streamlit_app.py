21a9291218fa0283e8118c1c29bcf530a13c932b
shreya4250 <146724974+shreya4250@users.noreply.github.com>
2023-10-02 16:43:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+Breakfast Menu
+   Omega 3 and Blueberry Oatmeal
+   kale,Spinach and Racket Smoothie
+   Hard-Boiled  Free-range Egg

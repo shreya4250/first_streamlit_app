289d5e91340cb8eeff6d727809e8642ce56d11b9
shreya4250 <146724974+shreya4250@users.noreply.github.com>
2023-10-03 16:50:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 # write your own comment -what does the next line do? 
